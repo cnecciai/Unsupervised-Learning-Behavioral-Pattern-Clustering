b19e24112c10af38b10af3f31e45113db1a74cf7
cnecciai <clarknecciai2@gmail.com>
2024-02-18 17:00:50 -0500

    Possible Recommendations??? Needs Robustness Check

--- Final_Project.R
+++ Final_Project.R
@@ -2,24 +2,12 @@
 #title: "Unsupervised Learning Final Project"
 #subtitle: 'PCA and Clustering'
 #authors: "Saxa Group #9"
-    #Kassandra Sellers - Preprocessing/Docx Formatting
-    #Mabel Lorena Barba - Preprocessing
-    #Benny Huang - Clustering(K-means)
-    #Tony Campoverde - Preprocessing/PCA
+    #Kassandra Sellers 
+    #Mabel Lorena Barba 
+    #Benny Huang 
+    #Tony Campoverde 
     #Clark Necciai
-
-#Outline: 
-    #Data Pre-processing
-        #Handling Missing Values
-    #Exploratory Data Analysis
-        #Principal Components Analysis
-        #K-Means Clustering
 #-----------------------------------
-#NOTE:
-
-#It may be the case that the full dataset might be performing so well would be because these reviews given shouldn't be looked
-#at as mainly outliers to simply be rid of. Maybe if there were minimal amounts, but these reviews are still valid. They aren't
-#invalid and so we shouldn't just arbitrarily throw them away. 
 
 #---Data Pre-Processing---
 
@@ -32,9 +20,10 @@ library(factoextra)
 library(PerformanceAnalytics)
 library(plotly)
 library(corrplot)
+library(NbClust) #NbClust Method
+library(clValid) #Silhouette
 
-#Read in Travel Data - CHANGE TO CSV
-#Travel_Data <- read_xlsx("Travel_Review.xlsx")
+#Read in Travel Data
 Travel_Data <- read_csv("Travel_Review.csv")
 #Initial  Inspection
 Travel_Data %>% glimpse()
@@ -48,61 +37,30 @@ duplicates <- Travel_Data[which(duplicated(Travel_Data)), ]
 if ((nrow(duplicates)) == 0) {
     print("No duplicates detected")}
 
-
-#Show Variables w/ Number of Missing Values (Train Set)
-print(paste("Percentage Missing Values for Dataset:"))
-Travel_miss <- sapply(Travel_Data, function(x){sum(is.na(x))})
-Travel_miss[Travel_miss != 0]
-
-#Visualize Missing Training Data
-vis_miss(Travel_Data) +
-    coord_flip()
-
-#---Missing Value Imputation---
-
-# Calculate median of "Gardens" column - Skewed Distribution
-median_gardens <- median(Travel_Data$Gardens, na.rm = TRUE)
-
-# Replace missing value with median
-Travel_Data$Gardens[is.na(Travel_Data$Gardens)] <- median_gardens
-
-# Verify the replacement
-missing_in_each_column <- colSums(is.na(Travel_Data))
-print(missing_in_each_column)
-
-hist(Travel_Data$Monuments)
-#------------------------------------end replacing w median-----
-
-#---Exploratory Data Analysis---
-
-# Taking Zero to mean no input/not interested, therefore skews data
-# towards a non-meaningful distribution... We're going to remove them 
-
 #---Function to Remove all Zero Values---
-remove_zeros <- function(x) {
+zero_to_na <- function(x) {
     x <- ifelse(x == 0, NA, x)
     return(x)
 }
 
 #Replace with NA Values
 Travel_Data_Non_Zero <- Travel_Data %>%
-    mutate(across(where(is.numeric), remove_zeros)) 
+    mutate(across(where(is.numeric), zero_to_na)) 
 
 #Visualize Missing Values
-vis_miss(Travel_Data_Non_Zero, cluster = T) + coord_flip()
+vis_miss(Travel_Data_Non_Zero) + coord_flip()
 
-replace_zeros <- function(var) {
-    median_value = median(var, na.rm = T)
-    newvar = replace_na(var, median_value)
-    return(newvar)
-}
+#Show Variables w/ Number of Missing Values (Train Set)
+print(paste("Percentage Missing Values for Dataset:"))
+Travel_miss <- sapply(Travel_Data_Non_Zero, function(x){sum(is.na(x))})
+Travel_miss[Travel_miss != 0]
 
+
+#Replace Missing Values (per variable) with median
 Travel_Data_Zero_Impute <- Travel_Data_Non_Zero %>%
     mutate_all(funs(ifelse(is.na(.), median(., na.rm = TRUE), .)))
 
-#Note: Knowing that K-Means & PCA can be sensitive to outliers, we're going 
-#to remove all outlier values from our full dataset and work with 2 datasets
-#Full Dataset and Another with All Outliers Removed to see the possible benefits
+#Creating a Second, Reduced Dataset for the Purposes of evaluating PCA and K-means Performance
 
 #---Function to Remove all Outliers---
 remove_outliers <- function(x) {
@@ -118,23 +76,18 @@ Travel_Data_No_Outliers <- Travel_Data_Zero_Impute %>%
     mutate(across(where(is.numeric), remove_outliers)) %>%
     drop_na()
 
-#Describe with Summary Statistics
-#UserID obviously useless here but useful for later
 
 Travel_Data_Full <- Travel_Data_Zero_Impute %>% select(-UserID) %>% data.frame()
 Travel_Data_Reduced <- Travel_Data_No_Outliers %>% select(-UserID) %>% data.frame()
 
+#Summary Statistics of Dataset with All Observations
 Travel_Data_Full %>% describe()
+
+#Summary Statistics of Dataset with Outlier Values Removed
 Travel_Data_Reduced %>% describe()
 
-#----
-#Currently we've imputed all zeros
-#Have two datasets
-#1. --- Travel_Data_Full --- Full Dataset with outliers + zero answers imputed
-#2. --- Travel_Data_Reduced ---  Outliers and zero answers imputed
-#----
 
-#Correlation Matrix
+#Correlation Matrix for Full Dataset
 corrplot::corrplot(cor(Travel_Data_Full),
                    type = "lower", diag = F, 
                    tl.col = "black", tl.cex = .7,
@@ -165,76 +118,50 @@ for (i in 1:ncol(Travel_Data_Full)) {
 #---Principal Components Analysis---
 
 #Extract Principal Components Through SVD
-Travel_PCA = prcomp(Travel_Data_Full, scale. = TRUE)
-
-#Extract Principal Components Through SVD
-Travel_PCA_Outlierless = prcomp(Travel_Data_Reduced, scale. = TRUE)
+Travel_PCA_Full = prcomp(Travel_Data_Full, scale. = TRUE)
+Travel_PCA_Reduced = prcomp(Travel_Data_Reduced, scale. = TRUE)
 
 #Show how the Variables are related to the Principal Components
-pca_var <- get_pca_var(Travel_PCA)
-pca_outlierless_var <- get_pca_var(Travel_PCA_Outlierless)
+pca_full_var <- get_pca_var(Travel_PCA_Full)
+pca_reduced_var <- get_pca_var(Travel_PCA_Reduced)
 
-corrplot(pca_var$contrib, is.corr = F, tl.col = "black",)
-corrplot(pca_outlierless_var$contrib, is.corr = F, tl.col = "black",)
+corrplot(pca_full_var$contrib, is.corr = F, tl.col = "black",)
+corrplot(pca_reduced_var$contrib, is.corr = F, tl.col = "black",)
 
 #Variable Contributions to Principal Components
-fviz_contrib(Travel_PCA, choice = "var", axes = 1) # Contributions of variables to PC1
-fviz_contrib(Travel_PCA_Outlierless, choice = "var", axes = 1) # Contributions of variables to PC1
+fviz_contrib(Travel_PCA_Full, choice = "var", axes = 1) # Contributions of variables to PC1
+fviz_contrib(Travel_PCA_Reduced, choice = "var", axes = 1) # Contributions of variables to PC1
 
-fviz_contrib(Travel_PCA, choice = "var", axes = 2) # Contributions of variables to PC2
-fviz_contrib(Travel_PCA_Outlierless, choice = "var", axes = 2) # Contributions of variables to PC2
-
+fviz_contrib(Travel_PCA_Full, choice = "var", axes = 2) # Contributions of variables to PC2
+fviz_contrib(Travel_PCA_Reduced, choice = "var", axes = 2) # Contributions of variables to PC2
 
 #Visualize explained variances per component
-fviz_eig(Travel_PCA, addlabels=TRUE)+
+fviz_eig(Travel_PCA_Full, addlabels=TRUE)+
     labs(x = "Dimensions (Pricipal Components)",
          title = "Scree-Plot for Full Dataset")
 
 #Actually Achieve better Variance for first few Principal Components
-fviz_eig(Travel_PCA_Outlierless, addlabels=TRUE)+
+fviz_eig(Travel_PCA_Reduced, addlabels=TRUE)+
     labs(x = "Dimensions (Pricipal Components)",
          title = "Scree-Plot for Dataset without Outliers")
     
-#MABEL-INSERT REASONING FOR REDUCTION HERE - Retain 80% of the Variance
-get_eig(Travel_PCA)
-ReducedPCA <- Travel_PCA$x[,1:13]
+#Retain Determined Percentage of Data (80%)
+get_eig(Travel_PCA_Full)
+ReducedPCA <- Travel_PCA_Full$x[,1:13]
 
-get_eig(Travel_PCA_Outlierless)
-ReducedPCA_Outlierless <- Travel_PCA_Outlierless$x[,1:11]
+get_eig(Travel_PCA_Reduced)
+ReducedPCA_Outlierless <- Travel_PCA_Reduced$x[,1:11]
 
-fviz_pca_var(Travel_PCA,  col.var = "contrib",
+fviz_pca_var(Travel_PCA_Full,  col.var = "contrib",
              gradient.cols = c("red", "darkgreen"),
              repel=TRUE,
              title = "Variables - PCA Full Dataset") 
 
 
-fviz_pca_var(Travel_PCA_Outlierless,  col.var = "contrib",
+fviz_pca_var(Travel_PCA_Reduced,  col.var = "contrib",
              gradient.cols = c("red", "darkgreen"),
              repel=TRUE,
              title = "Variables - PCA Outliers Removed")
-
-
-#Try two-dimensional plots of principal components
-Travel_PCA$x %>% data.frame() %>% ggplot(aes(PC1, PC2)) +
-    geom_point()
-
-
-Travel_PCA_Outlierless$x %>% data.frame() %>% ggplot(aes(PC1, PC2)) +
-    geom_point()
-
-
-#Show 3d dimensional plot of the full Travel Data
-#Hard to make out any spheres but there are some apparent
-#clusters, but mostly it appears as one big glob
-plot_ly(as.data.frame(Travel_PCA$x),
-        x=~PC1,y=~PC2,z=~PC3, size = 1.3) %>%
-    add_markers()
-
-#Removing the outliers from the full dataset actually 
-#seems to slim down the shape from the previous 3d plot
-plot_ly(as.data.frame(Travel_PCA_Outlierless$x),
-        x=~PC1,y=~PC2,z=~PC3, size = 1.3) %>%
-    add_markers()
 
 
 #---K-Means Clustering---
@@ -242,32 +169,50 @@ plot_ly(as.data.frame(Travel_PCA_Outlierless$x),
 #Three methods: Elbow, Silhouette, and Gap statistics
 
 #Elbow method - with maximum 5 clusters
-fviz_nbclust(ReducedPCA, kmeans, k.max=6, nstart=30, method="wss")
-fviz_nbclust(ReducedPCA_Outlierless, kmeans, k.max=6, nstart=30, method="wss")
+fviz_nbclust(ReducedPCA, kmeans, k.max=5, nstart=30, method="wss")
+fviz_nbclust(ReducedPCA_Outlierless, kmeans, k.max=5, nstart=30, method="wss")
 
 #Silhouette method - maximum 5 clusters
-fviz_nbclust(ReducedPCA, kmeans, k.max=6, nstart=30, method="silhouette")
-fviz_nbclust(ReducedPCA_Outlierless, kmeans, k.max=6, nstart=30, method="silhouette")
+fviz_nbclust(ReducedPCA, kmeans, k.max=5, nstart=30, method="silhouette")
+fviz_nbclust(ReducedPCA_Outlierless, kmeans, k.max=5, nstart=30, method="silhouette")
 
 #Compute gap statistics - maximum 5 clusters and 30 bootstrap samples
 set.seed(2023)
-fviz_nbclust(ReducedPCA, kmeans, k.max=6, nstart=30, method="gap_stat", nboot=30)
-fviz_nbclust(ReducedPCA_Outlierless, kmeans, k.max=6, nstart=30, method="gap_stat", nboot=30)
+fviz_nbclust(ReducedPCA, kmeans, k.max=5, nstart=30, method="gap_stat", nboot=30)
+fviz_nbclust(ReducedPCA_Outlierless, kmeans, k.max=5, nstart=30, method="gap_stat", nboot=30)
 
 #Verify Using NbClust for K-means --> Showing 3 as the best number of clusters
 nb_clust_full <- NbClust(ReducedPCA, distance = "euclidean", min.nc = 2, 
-                         max.nc = 6, method = "kmeans")
+                         max.nc = 5, method = "kmeans")
 
 #Overwhelmingly showing 17 supporting 3 as the optimal number of clusters
 nb_clust_reduced <- NbClust(ReducedPCA_Outlierless, distance = "euclidean", min.nc = 2,
-                            max.nc = 6, method = "kmeans")
+                            max.nc = 5, method = "kmeans")
+
+
+#Knowing that K-means is susceptible to outliers and that we've chosen 
+#to include the supposed outliers for our full dataset, we're going to run through
+#k-medoids with that dataset to see how the groupings may change (If at all)
+
+#Our full dataset actually appears to have very little change in groups.
+#This to us, suggests that our groupings are tight enough to warrant 
+#trusting in the original k-means cluster decision of having 3 groups
+
+#PAM Clustering
+pamclus = pam(ReducedPCA, 3)
+
+#3d Plot of cluster
+plot_ly(as.data.frame(ReducedPCA),
+        x=~PC1,y=~PC2,z=~PC3, color = pamclus$clustering,
+        colors = "Set1",
+        size = 4) %>%
+    add_markers()
 
 
 #Rerun K-means with Optimal K = 3 on Full Dataset K = 5 on Reduced
 
 set.seed(2024)
 k3_full = kmeans(ReducedPCA, centers=3, nstart=30)
-
 #--- Full Dataset Plots ---
 
 #2d Plot of Clusters
@@ -315,10 +260,45 @@ sile_full_outlierless = silhouette(k3_full_outlierless$cluster, dist(ReducedPCA_
 fviz_silhouette(sile_full_outlierless)
 
 
-#Based on everything I've seen so far, I'm inclined to believe that the full dataset with the "supposed"
-#outliers might actually be the preferred approach. There is an overwhelming consensus that the optimal
-#number of clusters is three. 
 
-#--Hierarchical---
+#NOW THAT WE HAVE MADE A DETERMINATION AS TO THE APPROPRIATE NUMBER OF CLUSTERS, 
+#WE SHOULD ATTACH THOSE LABELS TO THE ORIGINAL VARIABLES AND TRY AND SEE IF THERE
+#ARE ANY PATTERNS WE SHOULD CAPITALIZE ON TO GUIDE OUR MARKETING CAMPAIGN
+
+Cluster_And_Travel_Data <- cbind(Group = k3_full$cluster, Travel_Data_Zero_Impute)
+
+summary_df <- Cluster_And_Travel_Data %>%
+    group_by(Group) %>%
+    summarise(across(where(is.numeric), 
+                     list( median = median)))
+
+#Each Group Had a Median Rating... Let's Target Our Campaign
+highest_medians <- c()
+target_group <- c()
+consider_trip <- c()
+for (i in 2:ncol(summary_df)) {
+    var_name = summary_df[,i] %>% names()
+    max_for_var = (max(summary_df[,i]))
+    for (j in 1:3) {
+        if (summary_df[j,i] == max_for_var) {
+            target_group = c(target_group, j)
+            consider_trip = c(consider_trip, var_name)
+            highest_medians = c(highest_medians, max_for_var)
+        }
+    }
+}
+
+travel_guide <- data.frame(
+    "TargetGroup" = target_group,
+    "HighestMedianRating" = as.numeric(highest_medians),
+    "Corresponding Location" = str_remove(consider_trip, "_median")
+)
+
+
+#Use Sorted Travel Guide Information To target specific groups
+#for certain locations of interest
+travel_guide %>%
+arrange(TargetGroup, desc(HighestMedianRating))
+
 
 
