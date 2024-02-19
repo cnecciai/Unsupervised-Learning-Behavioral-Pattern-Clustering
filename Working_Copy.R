41c844f328d150ae5ea1c210ba800b7fb55d6b45
cnecciai <clarknecciai2@gmail.com>
2024-02-19 12:04:23 -0500

    Naming Adjustment/Additional Changes

--- Working_Copy.R
+++ Working_Copy.R
@@ -1,13 +1,48 @@
 #-----------------------------------
 #title: "Unsupervised Learning Final Project"
 #subtitle: 'PCA and Clustering'
-#authors: "Saxa Group #9"
-    #Kassandra Sellers 
-    #Mabel Lorena Barba 
-    #Benny Huang 
-    #Tony Campoverde 
-    #Clark Necciai
+# authors: "Saxa Group #9"
+# Kassandra Sellers
+# Mabel Lorena Barba
+# Benny Huang
+# Tony Campoverde
+# Clark Necciai
 #-----------------------------------
+
+# Questions we'll need to address in the 
+# final report: 
+# Why did we use PCA in conjunction with K-Means?
+# Why didn't we use Hierarchical Clustering? 
+# Assumptions of K-Means?
+# Assumptions of PCA?
+# How do we leverage PCA for K-Means?
+# What percentage of information did we wish to retain for PCA?
+# Why?
+
+
+# Overview of Final R Code:
+# Typical Pre-Processing:
+#   Overview of Variable Summary Statistics
+#   No Duplicate Values observed
+#   Missing Value (Gardens) Imputed --> Median skewed dist.
+# Treating Zero Values & Imputation of Median
+# Correlation Between Variables (Weak Correlation, May Require Many PCs for effective K-means)
+# ---Begin PCA---
+# Perform PCA
+# How PCs and Variables are Related
+# Visualize Scree-Plot
+# Show Variable Contributions (Maybe use this to verify end conclusions???)
+# ---Begin K-Means---
+# Utilize Different Performance Metrics to Determine the Optimal Number of Clusters from NbClust
+# Optimal Number of Clusters Determined to be 3
+# Visualize Clusters with 2D and 3D Plots to view Distinct Separation
+# Run PAM (K-Medoids) To see if there’s significant shift in the clusters (There’s not)
+# Show Silhouette
+# Map Cluster Labels Back to Original Dataset
+# Using our Cluster Labels:
+#     Find the Maximum Median Value for a variable and corresponding Group
+# Depending on value, consider using for Marketing Campaign
+
 
 #---Data Pre-Processing---
 
@@ -17,14 +52,15 @@ library(readxl)
 library(visdat)
 library(psych)
 library(factoextra)
-library(PerformanceAnalytics)
-library(plotly)
+library(plotly) #3D Plots
 library(corrplot)
+library(PerformanceAnalytics) #chart.Histogram
 library(NbClust) #NbClust Method
 library(clValid) #Silhouette
 
 #Read in Travel Data
 Travel_Data <- read_csv("Travel_Review.csv")
+
 #Initial  Inspection
 Travel_Data %>% glimpse()
 
@@ -37,7 +73,28 @@ duplicates <- Travel_Data[which(duplicated(Travel_Data)), ]
 if ((nrow(duplicates)) == 0) {
     print("No duplicates detected")}
 
-#---Function to Remove all Zero Values---
+#Show Variables w/ Number of Missing Values
+print(paste("Total Missing Values for Dataset:"))
+miss <- sapply(Travel_Data, function(x){sum(is.na(x))})
+miss[miss != 0]
+
+#Impute Median for Missing Value of Gardens
+Travel_Data[(is.na(Travel_Data$Gardens)), ]$Gardens = median(Travel_Data$Gardens, na.rm = T)
+
+#Verify no Missing Values
+miss <- sapply(Travel_Data, function(x){sum(is.na(x))})
+miss[miss != 0]
+
+# What do we hope to see/gain by imputing median for zero values? 
+
+# We're definitely looking to retain information. If an observation didn't give 
+# a rating to a particular location, we don't want to simply drop
+# the entire row which could contain useful information. In an effort 
+# to maintain the integrity of our variables distributions, knowing 
+# that they're skewed, we've chosen to impute the median. 
+
+#---Function to replace zeros with NA---
+
 zero_to_na <- function(x) {
     x <- ifelse(x == 0, NA, x)
     return(x)
@@ -50,7 +107,7 @@ Travel_Data_Non_Zero <- Travel_Data %>%
 #Visualize Missing Values
 vis_miss(Travel_Data_Non_Zero) + coord_flip()
 
-#Show Variables w/ Number of Missing Values (Train Set)
+#Show Variables w/ Number of Missing Values with Zero Treated as NA Values(Train Set)
 print(paste("Percentage Missing Values for Dataset:"))
 Travel_miss <- sapply(Travel_Data_Non_Zero, function(x){sum(is.na(x))})
 Travel_miss[Travel_miss != 0]
@@ -76,7 +133,7 @@ Travel_Data_No_Outliers <- Travel_Data_Zero_Impute %>%
     mutate(across(where(is.numeric), remove_outliers)) %>%
     drop_na()
 
-
+#Remove UserID in order to easily work with continuous variables
 Travel_Data_Full <- Travel_Data_Zero_Impute %>% select(-UserID) %>% data.frame()
 Travel_Data_Reduced <- Travel_Data_No_Outliers %>% select(-UserID) %>% data.frame()
 
@@ -85,7 +142,6 @@ Travel_Data_Full %>% describe()
 
 #Summary Statistics of Dataset with Outlier Values Removed
 Travel_Data_Reduced %>% describe()
-
 
 #Correlation Matrix for Full Dataset
 corrplot::corrplot(cor(Travel_Data_Full),
@@ -98,6 +154,16 @@ corrplot::corrplot(cor(Travel_Data_Reduced),
                    type = "lower", diag = F, 
                    tl.col = "black", tl.cex = .7,
                    tl.srt = 60)
+
+# Why are we choosing to retain all outliers?
+# 
+# We ultimately want to find groups that have distinct (hopefully)
+# high values of ratings for particular variables. If we choose 
+# to arbitrarily remove these observations, then we ultimately loose 
+# this information immediately from the start of our analysis. We'll 
+# (on top of K-means) utilize K-medoids in an attempt to verify that 
+# our determined K-means clusters are robustly determined enough to 
+# find reliable groupings from which to build our recommendations.
 
 #Visualize Histograms of Variables
 names <- colnames(Travel_Data_Full)
@@ -116,6 +182,12 @@ for (i in 1:ncol(Travel_Data_Full)) {
 }
 
 #---Principal Components Analysis---
+
+#How will we try to leverage Principal Component Analysis?
+
+#Want to extract useful information with a smaller degree of dimensionality.
+#K-means might try to incorrectly extrapolate groups in a high number of dimensions.
+#PCA can lower that dimensionality while retain useful information. 
 
 #Extract Principal Components Through SVD
 Travel_PCA_Full = prcomp(Travel_Data_Full, scale. = TRUE)
@@ -147,10 +219,10 @@ fviz_eig(Travel_PCA_Reduced, addlabels=TRUE)+
     
 #Retain Determined Percentage of Data (80%)
 get_eig(Travel_PCA_Full)
-ReducedPCA <- Travel_PCA_Full$x[,1:13]
+ReducedPCA <- Travel_PCA_Full$x[,1:18]
 
 get_eig(Travel_PCA_Reduced)
-ReducedPCA_Outlierless <- Travel_PCA_Reduced$x[,1:11]
+ReducedPCA_Outlierless <- Travel_PCA_Reduced$x[,1:16]
 
 fviz_pca_var(Travel_PCA_Full,  col.var = "contrib",
              gradient.cols = c("red", "darkgreen"),
@@ -190,23 +262,7 @@ nb_clust_reduced <- NbClust(ReducedPCA_Outlierless, distance = "euclidean", min.
                             max.nc = 5, method = "kmeans")
 
 
-#Knowing that K-means is susceptible to outliers and that we've chosen 
-#to include the supposed outliers for our full dataset, we're going to run through
-#k-medoids with that dataset to see how the groupings may change (If at all)
 
-#Our full dataset actually appears to have very little change in groups.
-#This to us, suggests that our groupings are tight enough to warrant 
-#trusting in the original k-means cluster decision of having 3 groups
-
-#PAM Clustering
-pamclus = pam(ReducedPCA, 3)
-
-#3d Plot of cluster
-plot_ly(as.data.frame(ReducedPCA),
-        x=~PC1,y=~PC2,z=~PC3, color = pamclus$clustering,
-        colors = "Set1",
-        size = 4) %>%
-    add_markers()
 
 
 #Rerun K-means with Optimal K = 3 on Full Dataset K = 5 on Reduced
@@ -229,6 +285,27 @@ plot_ly(as.data.frame(ReducedPCA),
         colors = "Set1",
         size = 4) %>%
     add_markers()
+
+
+#Knowing that K-means is susceptible to outliers and that we've chosen 
+#to include the supposed outliers for our full dataset, we're going to run through
+#k-medoids with that dataset to see how the groupings may change (If at all)
+
+#Our full dataset actually appears to have very little change in groups.
+#This to us, suggests that our groupings are tight enough to warrant 
+#trusting in the original k-means cluster decision of having 3 groups
+
+#PAM Clustering
+pamclus = pam(ReducedPCA, 3)
+
+#3d Plot of cluster
+plot_ly(as.data.frame(ReducedPCA),
+        x=~PC1,y=~PC2,z=~PC3, color = pamclus$clustering,
+        colors = "Set1",
+        size = 4) %>%
+    add_markers()
+
+
 
 #--- Reduced Dataset Plots ---
 
@@ -265,7 +342,7 @@ fviz_silhouette(sile_full_outlierless)
 #WE SHOULD ATTACH THOSE LABELS TO THE ORIGINAL VARIABLES AND TRY AND SEE IF THERE
 #ARE ANY PATTERNS WE SHOULD CAPITALIZE ON TO GUIDE OUR MARKETING CAMPAIGN
 
-Cluster_And_Travel_Data <- cbind(Group = k3_full$cluster, Travel_Data_Zero_Impute)
+Cluster_And_Travel_Data <- cbind(Group = factor(k3_full$cluster), Travel_Data_Zero_Impute)
 
 summary_df <- Cluster_And_Travel_Data %>%
     group_by(Group) %>%
@@ -300,5 +377,14 @@ travel_guide <- data.frame(
 travel_guide %>%
 arrange(TargetGroup, desc(HighestMedianRating))
 
+#---Extra Visualization for Group Ratings---
+
+#Inspect Variable of Choice
+Cluster_And_Travel_Data %>% 
+    ggplot(aes(Gardens, fill = Group)) +
+    geom_density(aes(alpha = 0.3)) +
+    labs(title = "Distribution of Group Median Ratings") +
+    theme_gray()
+    
 
 
